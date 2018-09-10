3065cc7c615106fac771c450746410fd15803f07
Felix Becker <felix.b@outlook.com>
2018-09-10 11:12:36 +0200

    feat(readme): add release instructions
    cc @beyang @sqs

--- src/cli.ts
+++ src/cli.ts
@@ -360,6 +360,11 @@ async function main(): Promise<void> {
             '',
             ...(hasTests ? ['## Test', '```', 'npm test', '```'] : []),
             '',
+            '## Release',
+            'Releases are done automatically in CI when commits are merged into master by analyzing [Conventional Commit Messages](https://conventionalcommits.org/).',
+            'After running `npm install`, commit messages will be linted automatically when committing though a git hook.',
+            "The git hook can be circumvented for fixup commits with [git's `fixup!` autosquash feature](https://fle.github.io/git-tip-keep-your-branch-clean-with-fixup-and-autosquash.html), or by passing `--no-verify` to `git commit`.",
+            'You may have to rebase a branch before merging to ensure it has a proper commit history, or squash merge with a manually edited commit message that conforms to the convention.',
         ].join('\n')
         await writeFile('README.md', readme)
     }
